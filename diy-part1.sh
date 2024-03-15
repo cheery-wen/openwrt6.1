38c9515ab1c504ed74fdd5dd7ab2947d7be54b84
cheery-wen <90075857+cheery-wen@users.noreply.github.com>
2024-03-15 14:53:00 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -45,7 +45,7 @@ git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git package/l
   
 git clone https://github.com/ElvenP/luci-app-onliner.git package/luci-app-onliner
 # svn co https://github.com/kenzok8/jell/trunk/luci-app-control-webrestriction package/luci-app-control-webrestriction
-git clone https://github.com/kenzok8/jell/tree/main/luci-app-control-webrestriction.git package/luci-app-control-webrestriction
+git clone https://github.com/kenzok8/jell/tree/main/luci-app-control-webrestriction package/luci-app-control-webrestriction
 
 # svn co https://github.com/kenzok8/jell/trunk/luci-app-netdata package/luci-app-netdata
 # git clone https://github.com/Jason6111/luci-app-netdata.git package/luci-app-netdata
