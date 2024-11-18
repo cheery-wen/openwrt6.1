b83cc6bb3305b8b8352708520831564f0735948d
cheery-wen <90075857+cheery-wen@users.noreply.github.com>
2024-11-18 11:32:17 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -32,7 +32,7 @@ sed -i '/uci commit system/i\uci set system.@system[0].hostname='OpenWrt'' packa
 
 
 # 版本号里显示一个自己的名字（281677160 build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些都是后增加的）
-sed -i "s/LEDE /Openwrt ($(TZ=UTC-8 date "+%Y.%m.%d") compiled by cheery)/g" package/lean/default-settings/files/zzz-default-settings
+sed -i "s/LEDE /OpenWrt ($(TZ=UTC-8 date "+%Y.%m.%d") compiled by cheery)/g" package/lean/default-settings/files/zzz-default-settings
 
 # 更换默认主题
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
