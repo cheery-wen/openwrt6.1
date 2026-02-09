dc7c85de178bd40aeba5c08ffc0e081487320201
cheery-wen <90075857+cheery-wen@users.noreply.github.com>
2026-02-09 15:27:23 +0800

    Fix clone URLs for OpenWrt Passwall packages

--- diy-part1.sh
+++ diy-part1.sh
@@ -77,8 +77,8 @@ git clone https://github.com/jerrykuku/luci-app-argon-config.git  package/luci-a
 #git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/openwrt-passwall
 #git clone https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
 
-git clone https://github.com/Openwrt-Passwall/openwrt-passwall-packages package/passwall-packages
-git clone https://github.com/Openwrt-Passwall/openwrt-passwall package/passwall-luci
+git clone https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git package/openwrt-passwall
+git clone https://github.com/Openwrt-Passwall/openwrt-passwall.git package/luci-app-passwall
 
 # git clone -b luci-smartdns-dev https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
 
